c684f6671e14fb11cbc7ac7deb39c29707f07b19
PradeepKAISCF <129294519+PradeepKAISCF@users.noreply.github.com>
2024-01-01 09:29:31 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API = axios.create({ baseURL: `http://localhost:5500/` });
-const API = axios.create({ baseURL: `https://null-yt-92e0a97ee0b2.herokuapp.com/` });
+const API = axios.create({ baseURL: `https://null-yt-c-2b31a7d828e5.herokuapp.com/` });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
